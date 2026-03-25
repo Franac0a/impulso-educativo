e6cf468a46cd90e245d5e0643679bc34f378bdd1
Facundo Cristaldo <facundocristaldo146@gmail.com>
2026-03-25 10:05:19 -0300

    antes de mergear a main

--- frontend/src/pages/CareerDetailPage.jsx
+++ frontend/src/pages/CareerDetailPage.jsx
@@ -70,7 +70,6 @@ export const CareerDetailPage = () => {
         </div>
       </div>
 
-      {/* Contenido */}
       <div className="container mx-auto px-6 mt-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Info Carrera */}
         <div className="lg:col-span-2 space-y-8">
